93bdf48e567c927fa36dee09dea4eedf4a5b91a1
vadivelan1998 <vadivelan1998@gmail.com>
2022-06-03 10:36:25 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
 import logo from './logo.svg';
 import './App.css';
+import {useEffect,useState} from "react"
 
 function App() {
+  const [data, setDataa] = useState([]);
+  const [page, setPage] = useState(1);
+  console.log(page,"pageee")
+  console.log(data,"dataaa")
+
+  useEffect(() => {
+    fetch(`https://api.instantwebtools.net/v1/passenger?=${page}&size=25`)
+      .then((res) => res.json())
+      .then((json) => setDataa([...data, ...json.data]));
+  }, [page]);
+
+  const scrollToEnd = () => {
+    setPage((page) => page + 1);
+  };
+
+  window.onscroll = function () {
+    console.log(window.innerHeight + document.documentElement.scrollTop);
+    console.log(document.documentElement.offsetHeight);
+    if (
+      window.innerHeight + document.documentElement.scrollTop ===
+      document.documentElement.offsetHeight
+    ) {
+      scrollToEnd();
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {data.length > 0 &&
+        data.map((elem, ind) => (
+          <div key={ind} className="container">
+            <div>ID:{elem._id}</div>
+            <div>NAME:{elem.name}</div>
+            <div>TRIPS:{elem.trips}</div>
+          </div>
+        ))}
     </div>
   );
 }
